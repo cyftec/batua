ae714c225c265df6a1070eb172f215ee0a5aeae5
chnkrydv <chnkrydv@gmail.com>
2025-07-23 22:18:35 +0530

    feat: Label component should have a large font variant

--- dev/@libs/elements/Label.ts
+++ dev/@libs/elements/Label.ts
@@ -3,17 +3,21 @@ import { component, m } from "@mufw/maya";
 
 type LabelProps = {
   cssClasses?: string;
+  size?: "large" | "small";
   unpadded?: boolean;
   text: string;
 };
 
-export const Label = component<LabelProps>(({ cssClasses, unpadded, text }) => {
-  const paddingCss = op(unpadded).ternary("pl0", "pl1");
-  const classes = tmpl`mt2 pt1 mb1 f7 light-silver ${paddingCss} ${cssClasses}`;
+export const Label = component<LabelProps>(
+  ({ cssClasses, size, unpadded, text }) => {
+    const paddingCss = op(unpadded).ternary("pl0", "pl1");
+    const sizeCss = op(size).equals("large").ternary("f6", "f7");
+    const classes = tmpl`mt2 pt1 mb1 light-silver ${paddingCss} ${sizeCss} ${cssClasses}`;
 
-  return m.Div({
-    onunmount: () => dispose(paddingCss, classes),
-    class: classes,
-    children: text,
-  });
-});
+    return m.Div({
+      onunmount: () => dispose(paddingCss, sizeCss, classes),
+      class: classes,
+      children: text,
+    });
+  }
+);
